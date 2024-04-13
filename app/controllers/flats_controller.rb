ba8a1c972221c2d9e114c7072771e3dfdbe613f6
karim issa <issa61139@gmail.com>
2024-04-13 17:00:09 -0400

    implemented the css, and the crud still working on the optional tasks

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,34 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, notice: 'Flat was successfully deleted.'
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.create!([
+  { name: 'Light & Spacious Garden Flat London', address: '10 Clifton Gardens London W9 1DT', description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory', price_per_night: 75, number_of_guests: 3 },
+  { name: 'Stylish House Close to River Thames', address: '5 Queensmill Road London SW6 6JP', description: 'Stylish house in a quiet district close to the Thames. Perfect for a family stay.', price_per_night: 90, number_of_guests: 4 },
+  { name: 'Modern Apartment Near the Louvre', address: '27 Rue de Rivoli Paris 75004', description: 'Enjoy the heart of Paris in this modern, well-equipped apartment with easy access to major sights.', price_per_night: 130, number_of_guests: 2 },
+  { name: 'Cozy Cottage in Bayswater', address: '34 Moscow Road London W2 4AH', description: 'A cozy and peaceful cottage perfect for weekends away, with quick access to central London.', price_per_night: 85, number_of_guests: 2 }
+])
